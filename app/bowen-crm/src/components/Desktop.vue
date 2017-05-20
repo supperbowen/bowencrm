536b7f71bcf59a60c81c7cdb338fc8ad01f1509e
supperbowen <bowenliang@outlook.com>
2017-05-21 00:11:47 +0800

    add element ui

---
+++ app/bowen-crm/src/components/Desktop.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-row >
+      Bowen's Crm
+    </el-row>
+    <el-row>
+      <el-col :xs="2" :md="2" :sm="2" :lg="2">
+
+      </el-col>
+      <el-col  :xs="10" :md="10" :sm="10" :lg="10">
+        <el-card class="box-card">
+          <div class="text item">
+            梁灿伦<br>15914321678
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row>
+      footer
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'desktop',
+    data () {
+      return {
+        msg: 'Bowen\'s app'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
--- app/bowen-crm/src/router/index.js
+++ app/bowen-crm/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Desktop from '@/components/Desktop'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'desktop',
+      component: Desktop
     }
   ]
 })
