9f92602301268d49ce111fb565f881052b3eff70
梁灿伦 <liangcl@nsw88.com>
2017-05-22 19:12:49 +0800

    添加员工表单

--- app/bowen-crm/src/modules/user/children/detail.vue
+++ app/bowen-crm/src/modules/user/children/detail.vue
@@ -1,46 +1,34 @@
 <template>
     <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-        <el-form-item label="活动名称" prop="name">
+        <el-form-item label="用户名称" prop="name">
             <el-input v-model="ruleForm.name"></el-input>
         </el-form-item>
-        <el-form-item label="活动区域" prop="region">
-            <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
+        <el-form-item label="用户权限" prop="region">
+            <el-select v-model="ruleForm.region" placeholder="请选择用户组">
+                <el-option label="业务员" value="shanghai"></el-option>
+                <el-option label="管理员" value="beijing"></el-option>
             </el-select>
         </el-form-item>
-        <el-form-item label="活动时间" required>
-            <el-col :span="11">
-                <el-form-item prop="date1">
-                    <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1"
-                                    style="width: 100%;"></el-date-picker>
-                </el-form-item>
-            </el-col>
-            <el-col class="line" :span="2">-</el-col>
-            <el-col :span="11">
-                <el-form-item prop="date2">
-                    <el-time-picker type="fixed-time" placeholder="选择时间" v-model="ruleForm.date2"
-                                    style="width: 100%;"></el-time-picker>
-                </el-form-item>
-            </el-col>
+        <el-form-item label="入职日期" required>
+            <el-form-item prop="date1">
+                <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1" style="width: 100%;"></el-date-picker>
+            </el-form-item>
         </el-form-item>
-        <el-form-item label="即时配送" prop="delivery">
-            <el-switch on-text="" off-text="" v-model="ruleForm.delivery"></el-switch>
-        </el-form-item>
-        <el-form-item label="活动性质" prop="type">
-            <el-checkbox-group v-model="ruleForm.type">
-                <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                <el-checkbox label="地推活动" name="type"></el-checkbox>
-                <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-            </el-checkbox-group>
-        </el-form-item>
-        <el-form-item label="特殊资源" prop="resource">
-            <el-radio-group v-model="ruleForm.resource">
-                <el-radio label="线上品牌商赞助"></el-radio>
-                <el-radio label="线下场地免费"></el-radio>
+        <el-form-item label="性别" prop="gender">
+            <el-radio-group v-model="ruleForm.gender">
+                <el-radio label="男"></el-radio>
+                <el-radio label="女"></el-radio>
             </el-radio-group>
         </el-form-item>
+
+        <el-form-item label="电话号码" prop="phone">
+            <el-input v-model="ruleForm.phone"></el-input>
+        </el-form-item>
+
+        <el-form-item label="贯籍" prop="src">
+            <el-input v-model="ruleForm.src"></el-input>
+        </el-form-item>
+
         <el-form-item label="活动形式" prop="desc">
             <el-input type="textarea" v-model="ruleForm.desc"></el-input>
         </el-form-item>
@@ -59,7 +47,7 @@
           region: '',
           date1: '',
           date2: '',
-          delivery: false,
+          gender: '男',
           type: [],
           resource: '',
           desc: ''
@@ -69,8 +57,12 @@
             {required: true, message: '请输入活动名称', trigger: 'blur'},
             {min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur'}
           ],
-          region: [
-            {required: true, message: '请选择活动区域', trigger: 'change'}
+          phone: [
+            {required: true, message: '请输入电话号码', trigger: 'blur'},
+            {min: 7, max: 12, message: '长度在 7 到 11 个字符', trigger: 'change'}
+          ],
+          src: [
+            {required: true, message: '请选择贯籍', trigger: 'change'}
           ],
           date1: [
             {type: 'date', required: true, message: '请选择日期', trigger: 'change'}
@@ -110,7 +102,8 @@
 
 <style lang="scss" scoped>
     @import "src/style/mixin";
-    .el-form{
+
+    .el-form {
         max-width: 600px;
     }
 </style>
